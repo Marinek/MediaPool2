e619b1d894bc2ed6a4c13af1d4a84547c1f8509e
Martin Gasse <martin.gasse@gmail.com>
2015-08-24 10:49:25 +0200

    @AuthenticationPrincipal
    added @AuthenticationPrincipal to movie controller

--- server/src/main/java/de/mediapool/server/media/movies/controller/MovieController.java
+++ server/src/main/java/de/mediapool/server/media/movies/controller/MovieController.java
@@ -8,6 +8,7 @@ import javax.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import de.mediapool.server.core.controller.MPController;
 import de.mediapool.server.media.movies.domain.MovieNodeDTO;
 import de.mediapool.server.media.movies.repository.MovieRepository;
+import de.mediapool.server.security.domain.MPUserDetails;
 
 @RestController	
 @RequestMapping("/rest/movie")
@@ -33,7 +35,7 @@ public class MovieController implements MPController {
 	}
 
 	@RequestMapping(value = "{id}", method = RequestMethod.GET)
-	public MovieNodeDTO getMovie(@PathVariable("id") String id, Principal  test) {
+	public MovieNodeDTO getMovie(@PathVariable("id") String id, @AuthenticationPrincipal MPUserDetails  test) {
 		logger.debug("Invoking: getMovie(id)");
 		
 		MovieNodeDTO movie = movieRepository.findById(id);
