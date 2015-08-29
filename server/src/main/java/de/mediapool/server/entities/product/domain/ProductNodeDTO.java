ee44821a0456c943c5aeafc444bdd34df3d45341
Manni204 <mm20482@googlemail.com>
2015-08-29 23:53:02 +0200

    relation movie / media

--- server/src/main/java/de/mediapool/server/entities/product/domain/ProductNodeDTO.java
+++ server/src/main/java/de/mediapool/server/entities/product/domain/ProductNodeDTO.java
@@ -1,46 +1,43 @@
 package de.mediapool.server.entities.product.domain;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Date;
 
-import org.neo4j.graphdb.Direction;
-import org.springframework.data.neo4j.annotation.Fetch;
 import org.springframework.data.neo4j.annotation.NodeEntity;
-import org.springframework.data.neo4j.annotation.RelatedTo;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import de.mediapool.server.core.domain.NodeDTO;
-import de.mediapool.server.entities.media.movies.domain.MovieNodeDTO;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-@SuppressWarnings("serial")
 @Getter
 @Setter
 @ToString
 @NodeEntity
 public abstract class ProductNodeDTO extends NodeDTO {
 
-	private String p_duration;
-	
-	private int p_age_restriction;
-	
-	
-	@JsonIgnore
-	@RelatedTo(type = "IS_ON", direction = Direction.INCOMING)
-	private @Fetch Set<MovieNodeDTO> movies;
-	
-	public void addPerson(MovieNodeDTO movie) {
-		if (movies == null) {
-			movies = new HashSet<MovieNodeDTO>();
-		}
-		movies.add(movie);
-	}
-	
-	
-	
-	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private String title;
+
+	private String p_orginaltitle;
+
+	private Date p_published;
+
+	private String p_special;
+
+	private String p_language;
+
+	private String p_price;
+
+	private String p_cover;
+
+	private String p_description;
+
+	private String p_ean;
+
+	private String p_format;
 
 }
