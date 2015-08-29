96f472c568409f14525f347413405a0d6d625183
Manni204 <mm20482@googlemail.com>
2015-08-29 22:56:40 +0200

    merged

--- server/src/main/java/de/mediapool/server/entities/media/movies/controller/MovieController.java
+++ server/src/main/java/de/mediapool/server/entities/media/movies/controller/MovieController.java
@@ -1,77 +1,6 @@
-<<<<<<< HEAD
 package de.mediapool.server.entities.media.movies.controller;
 
 import java.util.ArrayList;
-import java.util.List;
-
-import javax.annotation.PostConstruct;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-import de.mediapool.server.core.controller.MPController;
-import de.mediapool.server.entities.media.movies.domain.MovieNodeDTO;
-import de.mediapool.server.entities.media.movies.repository.MovieRepository;
-import de.mediapool.server.entities.users.domain.UserNodeDTO;
-import de.mediapool.server.security.domain.MPUserDetails;
-import de.mediapool.server.security.domain.PreAuthorization;
-
-@RestController
-@RequestMapping("/rest/movie")
-public class MovieController implements MPController {
-
-	private static final Logger logger = LoggerFactory.getLogger(MovieController.class);
-
-	@Autowired
-	private MovieRepository movieRepository;
-
-	@PostConstruct
-	public void init() {
-		logger.debug("Invoking: init()");
-	}
-
-	@PreAuthorize(PreAuthorization.ROLE_USER)
-	@RequestMapping(value = "{id}", method = RequestMethod.GET)
-	public MovieNodeDTO getMovie(@PathVariable("id") String id, @AuthenticationPrincipal MPUserDetails test) {
-		logger.debug("Invoking: getMovie(id)");
-
-		MovieNodeDTO movie = movieRepository.findById(id);
-
-		return movie;
-	}
-
-	@RequestMapping
-	public List<MovieNodeDTO> findMovieByName(String name) {
-		return new ArrayList<>();
-	}
-
-	@RequestMapping(value = "/create", method = RequestMethod.POST)
-	public MovieNodeDTO createMovie(@RequestBody MovieNodeDTO newMovie, @AuthenticationPrincipal UserNodeDTO currentUser) {
-		logger.debug("Invoking: createMovie(newMovie)");
-
-		if (newMovie.getId() != null) {
-			return newMovie;
-		}
-
-		MovieNodeDTO save = movieRepository.save(newMovie);
-
-		return save;
-	}
-
-}
-=======
-package de.mediapool.server.entities.media.movies.controller;
-
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -131,13 +60,9 @@ public class MovieController implements MPController {
 			return newMovie;
 		}
 
-		newMovie.setId(UUID.randomUUID().toString());
-
-
 		MovieNodeDTO save = movieRepository.save(newMovie);
 
 		return save;
 	}
 
 }
->>>>>>> origin/master
