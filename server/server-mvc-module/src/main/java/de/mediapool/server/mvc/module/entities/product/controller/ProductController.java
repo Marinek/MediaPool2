1dcca32e898ffde57d580b865c46b4539506ca25
Martin Gasse <martin.gasse@gmail.com>
2016-05-21 22:51:06 +0200

    added basis product creation form

--- server/server-mvc-module/src/main/java/de/mediapool/server/mvc/module/entities/product/controller/ProductController.java
+++ server/server-mvc-module/src/main/java/de/mediapool/server/mvc/module/entities/product/controller/ProductController.java
@@ -1,10 +1,14 @@
 package de.mediapool.server.mvc.module.entities.product.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.neo4j.conversion.Result;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import de.mediapool.server.core.controller.MPController;
@@ -14,6 +18,8 @@ import de.mediapool.server.entities.product.repository.ProductRepository;
 @Controller
 public class ProductController implements MPController {
 
+	private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
+	
 	@Autowired
 	private ProductRepository productRepository;
 	
@@ -33,4 +39,20 @@ public class ProductController implements MPController {
 		
 		return "views/products/fragmentProduct :: productDetail";
 	}
+
+    @RequestMapping(value="/editProduct", method=RequestMethod.GET)
+    public String greetingForm(Model model) {
+        model.addAttribute("product", new Product());
+        return "views/products/editProduct";
+    }
+	
+	@RequestMapping(value="/editProduct", method=RequestMethod.POST)
+	public String getProduct(@ModelAttribute Product product, Model model) {
+		logger.debug("Invoking: getProduct(product, model)");
+		model.addAttribute("product", productRepository.save(product));
+		
+		return "views/products/editProduct";
+	}
+	
+	
 }
